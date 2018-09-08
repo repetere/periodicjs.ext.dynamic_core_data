e4cee9d35e6fd58f577fdf64ba39861bc9f1c6b7
Jan Bialostok <janbialostok@gmail.com>
2018-09-07 23:37:43 -0400

    init

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const utilities = require('./utilities');
 const logger = periodic.logger;
 module.exports = () => {
   periodic.status.on('configuration-complete', (status) => {
+    console.log('~~~~~~~~~~~~~~~~~~~~');
+    console.log('~~~~~~~~~~~~~~~~~~~~');
+    console.log('calling dynamic database configuration');
+    console.log('~~~~~~~~~~~~~~~~~~~~');
+    console.log('~~~~~~~~~~~~~~~~~~~~');
     utilities.connect.connectDynamicDatabases()
       .then(() => {
         logger.silly('loaded dynamic databases');
